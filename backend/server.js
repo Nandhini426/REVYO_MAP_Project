1619ccfdba455112b1a286abbaf99ad294d1fe1e
NANDHINI R <nandhinirajeshwari1432@gmail.com>
2026-04-15 01:20:28 +0530

    Update CORS middleware to specify origin and credentials

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 // ── Middleware ───────────────────────────
-app.use(cors());
+app.use(cors({
+  origin: "https://revyo-map-project.vercel.app",
+  credentials: true
+}));
 app.use(express.json({ limit: '10mb' }))
 
 // ── API Routes ──────────────────────────
